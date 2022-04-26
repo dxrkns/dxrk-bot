bcec22d5db2540ac3ec6fab1d9028f0422abb149
iamvishalchandra <vishal.chandra75@gmail.com>
2022-04-26 17:30:57 +0530

    fixed attachment issues

--- src/helper/message/embedMessage.ts
+++ src/helper/message/embedMessage.ts
@@ -1,8 +1,12 @@
-import { Message, MessageEmbed, Util } from "discord.js";
+import { Message, MessageAttachment, MessageEmbed, Util } from "discord.js";
+import { bot } from "../../bot";
 import { EmbedChannelDB } from "../../database/models/modelsIndex";
 
 export const embedMessage = async (message: Message<boolean>) => {
   try {
+    if (message.author.id === bot.user.id) return;
+    console.log(message);
+
     if (
       message.channel.type !== "GUILD_TEXT" &&
       message.channel.type !== "GUILD_NEWS"
@@ -21,19 +25,36 @@ export const embedMessage = async (message: Message<boolean>) => {
         outputChannel.type !== "GUILD_NEWS")
     )
       return;
-
+    const attachments = message.attachments.map((attachment) => attachment);
     const messages = Util.splitMessage(message.content);
 
     const author = message.author;
-    messages.forEach((message, i) => {
-      const channelEmbed = new MessageEmbed()
-        .setAuthor({
-          name: author.username,
-          iconURL: author.avatarURL(),
-        })
-        .setDescription(message);
-
-      outputChannel.send({ embeds: [channelEmbed] });
+    if (message.content) {
+      messages.forEach((message, i) => {
+        const channelEmbed = new MessageEmbed()
+          .setAuthor({
+            name: author.username,
+            iconURL: author.avatarURL(),
+          })
+          .setDescription(message);
+        outputChannel.send({ embeds: [channelEmbed] });
+      });
+    }
+    if (attachments.length < 1) return;
+    attachments.forEach(async (attachment, i) => {
+      try {
+        const attachmentEmbed = new MessageEmbed()
+          .setAuthor({
+            name: author.username,
+            iconURL: author.avatarURL(),
+          })
+          .addField(`\u200b`, `Attachemnt ${i + 1} - ${attachment.url}`);
+        await outputChannel.send({
+          embeds: [attachmentEmbed],
+        });
+      } catch (error) {
+        return console.log(error);
+      }
     });
   } catch (error) {
     return console.log(error);
