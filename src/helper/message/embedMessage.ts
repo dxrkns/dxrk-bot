aea7ac155699c44bbf829e90709f1ab3719c4931
iamvishalchandra <vishal.chandra75@gmail.com>
2022-04-27 09:29:00 +0530

    added support for stickers

--- src/helper/message/embedMessage.ts
+++ src/helper/message/embedMessage.ts
@@ -25,6 +25,7 @@ export const embedMessage = async (message: Message<boolean>) => {
     )
       return;
     const attachments = message.attachments.map((attachment) => attachment);
+    const stickers = message.stickers.map((sticker) => sticker);
     const messages = Util.splitMessage(message.content);
 
     const author = message.author;
@@ -39,22 +40,37 @@ export const embedMessage = async (message: Message<boolean>) => {
         outputChannel.send({ embeds: [channelEmbed] });
       });
     }
-    if (attachments.length < 1) return;
-    attachments.forEach(async (attachment, i) => {
-      try {
-        const attachmentEmbed = new MessageEmbed()
+    if (attachments.length > 0) {
+      attachments.forEach(async (attachment, i) => {
+        try {
+          const attachmentEmbed = new MessageEmbed()
+            .setAuthor({
+              name: author.username,
+              iconURL: author.avatarURL(),
+            })
+            .addField(`\u200b`, `Attachment - ${attachment.url}`);
+          await outputChannel.send({
+            embeds: [attachmentEmbed],
+          });
+        } catch (error) {
+          return console.log(error);
+        }
+      });
+    }
+
+    if (stickers.length > 0) {
+      stickers.forEach(async (sticker, i) => {
+        const stickerEmbed = new MessageEmbed()
           .setAuthor({
             name: author.username,
             iconURL: author.avatarURL(),
           })
-          .addField(`\u200b`, `Attachemnt ${i + 1} - ${attachment.url}`);
+          .addField(`\u200b`, `Sticker - ${sticker.name}`);
         await outputChannel.send({
-          embeds: [attachmentEmbed],
+          embeds: [stickerEmbed],
         });
-      } catch (error) {
-        return console.log(error);
-      }
-    });
+      });
+    }
   } catch (error) {
     return console.log(error);
   }
