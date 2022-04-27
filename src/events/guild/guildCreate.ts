7157319ead9ea340fdf5f2ef2c2eb1451a776918
iamvishalchandra <vishal.chandra75@gmail.com>
2022-04-27 09:37:18 +0530

    added guild create

---
+++ src/events/guild/guildCreate.ts
@@ -0,0 +1,15 @@
+import { bot } from "../../bot";
+import { Event } from "../../structures/Events";
+import { setGuildPermissions } from "../../structures/SetPermissions";
+
+export default new Event("guildCreate", async (guild) => {
+  try {
+    const commands = bot.commands.map((cmd) => cmd);
+    guild.commands.set(commands).then((commandCollection) => {
+      setGuildPermissions({ guild, commands, commandCollection });
+    });
+    console.log(`Registered commands ${commands.length} to ${guild.name}..`);
+  } catch (error) {
+    return console.log(error);
+  }
+});
