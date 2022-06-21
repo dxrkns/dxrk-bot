501ee24fdde3ad551de89b24ff96696c7af493a7
iamvishalchandra <vishal.chandra75@gmail.com>
2022-06-21 14:40:22 +0530

    removed commid residue

--- src/typings/Client.type.ts
+++
@@ -1,6 +0,0 @@
-import { ApplicationCommandDataResolvable } from "discord.js";
-import { CommandType } from "./Command.type";
-export interface RegisterCommandsOptions {
-  guildId?: string;
-  commands: CommandType[];
-}
